a83d32cd69736ead784e4d4bd1087c33ee842e29
Joannis orlandos <joannis@orlandos.nl>
2021-09-14 17:49:23 +0200

    Add test

--- Tests/MongoQueueTests/MongoQueueTests.swift
+++ Tests/MongoQueueTests/MongoQueueTests.swift
@@ -1,8 +1,35 @@
-    import XCTest
-    @testable import MongoQueue
+import XCTest
+import MongoKitten
+@testable import MongoQueue
 
-    final class MongoQueueTests: XCTestCase {
-        func testExample() {
-            
-        }
+final class MongoQueueTests: XCTestCase {
+    func testExample() async throws {
+        let db = try MongoDatabase.synchronousConnect("mongodb://localhost/queues")
+        let queue = MongoQueue(collection: db["tasks"])
+        queue.registerTask(_Task.self, context: ())
+        try await queue.queueTask(_Task(message: "Test0"))
+        try queue.runInBackground()
+        try await queue.queueTask(_Task(message: "Test1"))
+        try await queue.queueTask(_Task(message: "Test2"))
+        try await queue.queueTask(_Task(message: "Test3"))
+        
+        // Sleep 5 sec
+        await Task.sleep(5_000_000_000)
     }
+}
+
+struct _Task: ScheduledTask {
+    var scheduledDate: Date {
+        Date()
+    }
+    
+    let message: String
+    
+    func execute(withContext context: Void) async throws {
+        print(message)
+    }
+    
+    func onExecutionFailure(failureContext: QueuedTaskFailure<()>) async throws -> TaskExecutionFailureAction {
+        return .dequeue()
+    }
+}
