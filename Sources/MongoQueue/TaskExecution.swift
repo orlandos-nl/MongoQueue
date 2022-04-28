5d501c7ff3a8a53ae14b4036f36099e543145a79
Joannis Orlandos <joannis@orlandos.nl>
2022-04-28 18:57:59 +0200

    Support soft-deletes

--- Sources/MongoQueue/TaskExecution.swift
+++ Sources/MongoQueue/TaskExecution.swift
@@ -1,24 +1,51 @@
 import Foundation
 
+public struct TaskRemovalAction {
+    enum _Raw {
+        case dequeue, softDelete
+    }
+    
+    let raw: _Raw
+    
+    public static func dequeue() -> TaskRemovalAction {
+        TaskRemovalAction(raw: .dequeue)
+    }
+    
+    public static func softDelete() -> TaskRemovalAction {
+        TaskRemovalAction(raw: .softDelete)
+    }
+}
+
 public struct TaskExecutionFailureAction {
     enum _Raw {
-        case dequeue
-        case retry(maxAttempts: Int?)
-        case retryAfter(TimeInterval, maxAttempts: Int?)
+        case removal(TaskRemovalAction)
+        case retry(maxAttempts: Int?, action: TaskRemovalAction)
+        case retryAfter(TimeInterval, maxAttempts: Int?, action: TaskRemovalAction)
     }
     
     let raw: _Raw
     
     public static func dequeue() -> TaskExecutionFailureAction {
-        TaskExecutionFailureAction(raw: .dequeue)
+        TaskExecutionFailureAction(raw: .removal(.dequeue()))
     }
     
-    public static func retry(maxAttempts: Int?) -> TaskExecutionFailureAction {
-        TaskExecutionFailureAction(raw: .retry(maxAttempts: maxAttempts))
+    public static func softDelete() -> TaskExecutionFailureAction {
+        TaskExecutionFailureAction(raw: .removal(.softDelete()))
     }
     
-    public static func retryAfter(_ interval: TimeInterval, maxAttempts: Int?) -> TaskExecutionFailureAction {
-        TaskExecutionFailureAction(raw: .retryAfter(interval, maxAttempts: maxAttempts))
+    public static func retry(
+        maxAttempts: Int?,
+        action: TaskRemovalAction = .dequeue()
+    ) -> TaskExecutionFailureAction {
+        TaskExecutionFailureAction(raw: .retry(maxAttempts: maxAttempts, action: action))
+    }
+    
+    public static func retryAfter(
+        _ interval: TimeInterval,
+        maxAttempts: Int?,
+        action: TaskRemovalAction = .dequeue()
+    ) -> TaskExecutionFailureAction {
+        TaskExecutionFailureAction(raw: .retryAfter(interval, maxAttempts: maxAttempts, action: action))
     }
 }
 
